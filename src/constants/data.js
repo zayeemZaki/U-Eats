4142bd646f29defa4ec838c3e06bbfd48d450925
zayeemZaki <137522702+zayeemZaki@users.noreply.github.com>
2023-11-21 16:52:08 -0500

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -25,6 +25,7 @@ module.exports = {
       "stripePriceId": "price_1OEK1PFGH3BedVinYTfgkg4F"
     }
   ],
+
   "specialSandwiches": [
     {
       "title": "Chicken Tawook",
@@ -50,5 +51,260 @@ module.exports = {
       ],
       "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
     }
+  ],
+
+  "starters": [
+    {
+      "title": "French Fries",
+      "price": "$4.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Cheese Sticks",
+      "price": "$8.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Onion Rings",
+      "price": "$7.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Cheese Cubes",
+      "price": "$10.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Small Hummus",
+      "price": "$7.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Large Hummus",
+      "price": "$9.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Grape Leaves",
+      "price": "$8.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Gyro Fries",
+      "price": "$11.99",
+      "tags": [
+        "Describe the item"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    }
+  ],
+
+  "wings": [
+    {
+      "title": "Rocket Chicken",
+      "price": "$10",
+      "tags": [
+        "10 Wings"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Rocket Chicken",
+      "price": "$19",
+      "tags": [
+        "20 Wings"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Rocket Chicken",
+      "price": "$45",
+      "tags": [
+        "50 Wings"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title": "Chunky Chicken",
+      "price": "$9.99",
+      "tags": [
+        "1 Pound of our Halal boneless chicken seasoned and fried to perfection"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title":  "Chix Strips",
+      "price": "$5",
+      "tags": [
+        "3 piece of our Halal Chicken Tenders fried to a golden crisp"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    },
+    {
+      "title":  "Chix Strips",
+      "price": "$9",
+      "tags": [
+        "6 piece of our Halal Chicken Tenders fried to a golden crisp"
+      ],
+      "stripePriceId": "Unique Id from stripe"
+    }
+  ],
+
+  "sandwiches": [
+    {
+      "title": "Chicken Tawook",
+      "price": "$8.99",
+      "tags": [
+        "Delicious seasoned chicken wrapped in pita bread"
+      ],
+      "stripePriceId": "price_1OEK2GFGH3BedVinQRjly8V7"
+    },
+    {
+      "title": "Falafel Sandwich",
+      "price": "$8.99",
+      "tags": [
+        "Fried Falafel in pita bread"
+      ],
+      "stripePriceId": "price_1OEK2kFGH3BedVinKlyBAGvj"
+    },
+    {
+      "title": "Chicken Sandwich",
+      "price": "$9.99",
+      "tags": [
+        "Perfectly seasoned chicken breast(Grilled or Crispy), letttuce, tomato, & mayo served on a bun"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Gyro",
+      "price": "$9.99",
+      "tags": [
+        "Your choice of lamb gyro meat or chicken, onions, tomatoes, and tzatziki sauce wrapped in fresh pita bread"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Patty Melt",
+      "price": "$9.99",
+      "tags": [
+        "Our Halal beef patty, sauteed onions, American & Swiss cheese on rye bread"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Fish Sandwich",
+      "price": "$9.99",
+      "tags": [
+        "Delicious filet of cod on a bun"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    }
+  ],
+  "burgers": [
+    {
+      "title": "Single Burger",
+      "price": "$9.99",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Double Burger",
+      "price": "$12.99",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    }, 
+    {
+      "title": "Add Fries",
+      "price": "$3",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Add Turkey",
+      "price": "$3",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Add Beef Bacon",
+      "price": "$3",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Add Cheese",
+      "price": "$1",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    }
+  ],
+
+  "salads": [
+    {
+      "title": "Small House Salad",
+      "price": "$6.99",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Large House Salad",
+      "price": "$9.99",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Small Greek Salad",
+      "price": "$8.99",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    },
+    {
+      "title": "Large Greek Salad",
+      "price": "$12.99",
+      "tags": [
+        " Write description"
+      ],
+      "stripePriceId": "price_1OEK3EFGH3BedVinWMUvHS9Q"
+    }
   ]
+
 };
