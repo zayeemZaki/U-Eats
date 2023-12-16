a3af5b654e455d6bdc725cacaba79be8cbcba048
zayeemZaki <zayeemzaki45@gmail.com>
2023-12-16 17:00:59 -0500

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Amplify } from "aws-amplify";
+import Amplify, { API } from 'aws-amplify';
 import amplifyconfig from './amplifyconfiguration.json';
 
 Amplify.configure(amplifyconfig);
