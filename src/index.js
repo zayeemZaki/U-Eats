883b427f880a234012336e204938fcc0de369d96
zayeemZaki <zayeemzaki45@gmail.com>
2023-12-16 17:35:52 -0500

    .

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Amplify, { API } from 'aws-amplify';
+import { Amplify } from "aws-amplify";
 import amplifyconfig from './amplifyconfiguration.json';
 
 Amplify.configure(amplifyconfig);
-API.configure(awsconfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
