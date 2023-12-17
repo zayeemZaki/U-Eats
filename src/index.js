a0539f3281149863687849bdf1192170c62ea68f
zayeemZaki <zayeemzaki45@gmail.com>
2023-12-16 23:40:45 -0500

    .

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import App from './App';
 import { Amplify } from "aws-amplify";
 import amplifyconfig from './amplifyconfiguration.json';
 import awsmobile from './aws-exports';
-Amplify.configure(amplifyconfig);
-
+import awsconfig from './aws-exports';
+//Amplify.configure(amplifyconfig);
+Amplify.configure(awsconfig);
+const existingConfig = Amplify.getConfig();
+Amplify.configure({
+  ...existingConfig,
+  API: {
+    ...existingConfig.API?.REST,
+    'stripeAPI': {
+      endpoint: 'https://sapsmxfq24.execute-api.us-east-1.amazonaws.com',
+      region: 'us-east-1'
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
