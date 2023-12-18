a0951ab794d2e694c11874b6549752a3bdab3b80
zayeemZaki <zayeemzaki45@gmail.com>
2023-12-18 17:27:13 -0500

    .

--- src/index.js
+++ src/index.js
@@ -4,22 +4,24 @@ import './index.css';
 import App from './App';
 import { Amplify } from "aws-amplify";
 import amplifyconfig from './amplifyconfiguration.json';
-import awsmobile from './aws-exports';
-import awsconfig from './aws-exports';
+//import awsmobile from './aws-exports';
+//import awsconfig from './aws-exports';
 Amplify.configure(amplifyconfig);
-Amplify.configure(awsmobile);
-Amplify.configure(awsconfig);
+//Amplify.configure(awsconfig);
 
 const existingConfig = Amplify.getConfig();
 Amplify.configure({
   ...existingConfig,
   API: {
-    ...existingConfig.API?.REST,
-    'stripeAPI': {
-      endpoint: 'https://sapsmxfq24.execute-api.us-east-1.amazonaws.com/dev',
-      region: 'us-east-1'
+    ...existingConfig.API,
+    REST: {
+      ...existingConfig.API?.REST,
+      'stripeAPI': {
+        endpoint: 'https://sapsmxfq24.execute-api.us-east-1.amazonaws.com/dev',
+        region: 'us-east-1'
+      }
     }
-  }
+  },
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
